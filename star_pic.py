0d0485215e304731ba836d6e3b7a7d44ce54ace3
roger <rogerluo387@gmail.com>
2018-07-20 00:06:21 +0800

    Create star_pic.py

---
+++ star_pic.py
@@ -0,0 +1,11 @@
+def drawStar():
+    plt.figure(figsize=(8, 4))
+    x = np.random.random(100)
+    y = np.random.random(100)
+    plt.scatter(x, y, s=x * 1000, c='y', marker='^', alpha=0.5, lw=2, edgecolors='blue')
+    plt.xlim(0, 1)
+    plt.ylim(0, 1)
+    plt.show()
+
+
+drawStar()
